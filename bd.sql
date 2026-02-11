678fb0386c0e0759074717a5383d1fbc7d2e7b9a
jairojg5 <901439@unizar.es>
2026-02-11 12:41:50 +0100

    Creacion tabla Partida y rol

--- bd.sql
+++ bd.sql
@@ -1,1 +1,21 @@
+CREATE TABLE PARTIDA(
+    id_partida INT PRIMARY KEY,
+    num_cartas_inicio INT NOT NULL,
+    modo_cartas_especiales BOOLEAN NOT NULL,
+    modo_roles BOOLEAN NOT NULL,
+    sonido BOOLEAN NOT NULL,
+    musica BOOLEAN NOT NULL,
+    vibracion BOOLEAN NOT NULL,
+    carta_actual_id INT NOT NULL,
+    pausa BOOLEAN NOT NULL,
+    FOREIGN KEY (carta_actual_id) REFERENCES CARTA(id_carta)
+
+)
+
+CREATE TABLE ROL(
+    id_rol INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+    num_usos_rol INT NOT NULL
+)
+
 
