4accc5209ce90cff1898347e968cc2711f5a934c
a896797 <896797@unizar.es>
2026-02-18 12:04:31 +0100

    BaseActualizada

--- bd.sql
+++ bd.sql
@@ -7,15 +7,18 @@ CREATE TABLE PARTIDA(
     musica BOOLEAN NOT NULL,
     vibracion BOOLEAN NOT NULL,
     carta_actual_id INT NOT NULL,
-    pausa BOOLEAN NOT NULL,
-    acabada BOOLEAN NOT NULL,
+    estado VARCHAR(20) NOT NULL CHECK (estado IN ('en curso','pausada', 'finalizada')),
+    orden_turnos []INT NOT NULL,
+    sentido INT NOT NULL CHECK (sentido IN (1, -1)),
+    timeout_turno INT NOT NULL,
     FOREIGN KEY (carta_actual_id) REFERENCES CARTA(id_carta)
 
 );
 
 CREATE TABLE ROL(
     id_rol INT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
+    imagen TEXT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
     num_usos_max INT NOT NULL
 );
 
@@ -25,7 +28,8 @@ CREATE TABLE USUARIO_EN_PARTIDA(
     id_partida INT,
     usos_rol_partida INT NOT NULL,
     total_cartas INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(username),
+    id_rol INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES USUARIO(nombre_usuario),
     FOREIGN KEY (id_partida) REFERENCES PARTIDA(id_partida),
     FOREIGN KEY (id_rol) REFERENCES ROL(id_rol)
 );
@@ -42,17 +46,17 @@ CREATE TABLE AMIGOS (
     id_usuario1 VARCHAR(50),
     id_usuario2 VARCHAR(50),
     PRIMARY KEY (id_usuario1, id_usuario2),
-    FOREIGN KEY (id_usuario1) REFERENCES Usuario(username),
-    FOREIGN KEY (id_usuario2) REFERENCES Usuario(username)
+    FOREIGN KEY (id_usuario1) REFERENCES USUARIO(nombre_usuario),
+    FOREIGN KEY (id_usuario2) REFERENCES USUARIO(nombre_usuario)
 );
 
 CREATE TABLE SOLICITUD_AMISTAD (
-    id_solicitud INT PRIMARY KEY,
     id_usuario_origen VARCHAR(50),
     id_usuario_destino VARCHAR(50),
     estado VARCHAR(100) NOT NULL CHECK (estado IN ('pendiente', 'aceptada', 'rechazada')),
-    FOREIGN KEY (id_usuario_origen) REFERENCES Usuario(username),
-    FOREIGN KEY (id_usuario_destino) REFERENCES Usuario(username)
+    FOREIGN KEY (id_usuario_origen) REFERENCES USUARIO(nombre_usuario),
+    FOREIGN KEY (id_usuario_destino) REFERENCES USUARIO(nombre_usuario),
+    PRIMARY KEY (id_usuario_origen, id_usuario_destino)
 );
 
 
@@ -62,7 +66,7 @@ CREATE TABLE SOLICITUD_AMISTAD (
 
 
 CREATE TABLE USUARIO (
-    id_usuario VARCHAR(50) PRIMARY KEY,
+    nombre_usuario TEXT PRIMARY KEY,
     contrasena VARCHAR(255) NOT NULL, 
     correo VARCHAR(255) NOT NULL UNIQUE,
     monedas INTEGER DEFAULT 0, 
@@ -73,18 +77,17 @@ CREATE TABLE USUARIO (
     numero_amigos INTEGER DEFAULT 0,
     numero_solicitudes INTEGER DEFAULT 0,
     
-    avatares_comprados INTEGER[],
-    estilos_compradas INTEGER[],
-
     id_avatar_seleccionado INTEGER,
-    id_estilo_seleccionada INTEGER,
+    id_estilo_seleccionadO INTEGER,
 
-    FOREIGN KEY (avatares_comprados) REFERENCES AVATAR(id_avatar),
+    FOREIGN KEY (id_avatar_seleccionado) REFERENCES AVATAR(id_avatar),
+    FOREIGN KEY (id_estilo_seleccionadO) REFERENCES ESTILO_AVATAR(id_estilo)
 );
-
 
 CREATE TABLE CARTA(
     id_carta SERIAL PRIMARY KEY,
+    id_estilo INT NOT NULL,
+    FOREIGN KEY (id_estilo) REFERENCES ESTILO(id_estilo) ON DELETE CASCADE
 );
 
 CREATE TABLE CARTA_DE_NUMERO(
@@ -112,18 +115,49 @@ CREATE TABLE AVATAR(
     muestoAvatar BOOLEAN NOT NULL
 );
 
-CREATE TABLE ESTILO_AVATAR(
+CREATE TABLE ESTILO(
     id_estilo SERIAL PRIMARY KEY,
+    fondo TEXT NOT NULL,
+    reverso TEXT NOT NULL,
     muestroEstilo BOOLEAN NOT NULL
 );
 
-CREATE TABLE USUARIO_PARTIDA_ROL(
-
-    pk_usuario_partida_rol SERIAL PRIMARY KEY,
-    id_usuario VARCHAR(50) NOT NULL,
+CREATE TABLE MAZO (
+    id_mazo SERIAL PRIMARY KEY,
     id_partida INT NOT NULL,
+    FOREIGN KEY (id_partida) REFERENCES PARTIDA(id_partida) ON DELETE CASCADE
+);
 
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-    FOREIGN KEY (id_partida) REFERENCES PARTIDA(id_partida)
-    CONSTRAINT pk_usuario_partida_rol PRIMARY KEY (id_usuario, id_partida)
-)
+CREATE TABLE MAZO_EN_JUEGO(
+    id_mazo INT NOT NULL,
+    id_carta INT NOT NULL,
+    orden INT NOT NULL,
+    PRIMARY KEY (id_mazo, id_carta),
+    FOREIGN KEY (id_mazo) REFERENCES MAZO(id_mazo),
+    FOREIGN KEY (id_carta) REFERENCES CARTA(id_carta) 
+
+);
+
+CREATE TABLE CARTAS_USUARIO(
+    id_partidaUsuario VARCHAR(50) NOT NULL,
+    id_carta INT NOT NULL,
+    PRIMARY KEY (id_partidaUsuario, id_carta),
+    FOREIGN KEY (id_partidaUsuario) REFERENCES USUARIO_EN_PARTIDA(id_partidaUsuario),
+    FOREIGN KEY (id_carta) REFERENCES CARTA(id_carta)
+);
+
+CREATE TABLE AVATARES_COMPRADOS(
+    nombre_usuario VARCHAR(50) NOT NULL,
+    id_avatar  INT NOT NULL,
+    PRIMARY KEY (nombre_usuario, id_avatar),
+    FOREIGN KEY (nombre_usuario) REFERENCES USUARIO(nombre_usuario),
+    FOREIGN KEY (id_avatar) REFERENCES AVATAR(id_avatar)
+);
+
+CREATE TABLE ESTILOS_COMPRADOS(
+    nombre_usuario VARCHAR(50) NOT NULL,
+    id_estilos  INT NOT NULL,
+    PRIMARY KEY (nombre_usuario, id_avatar),
+    FOREIGN KEY (nombre_usuario) REFERENCES USUARIO(nombre_usuario),
+    FOREIGN KEY (id_estilos) REFERENCES ESTILO(id_estilo)
+);
