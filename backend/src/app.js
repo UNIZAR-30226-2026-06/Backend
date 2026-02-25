be26d7b1882852cad346b63fb6d7371676b9f291
nekanediaz <116024637+nekanediaz@users.noreply.github.com>
2026-02-25 12:20:24 +0100

    update ap.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,6 +13,33 @@ app.get('/api/v1/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+// Configuración de Swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de NotUno',
+      version: '1.0.0',
+      description: 'Documentación de la API para el juego NotUno',
+    },
+    servers: [{ url: 'http://localhost:3000' }],
+  },
+  // Le decimos a Swagger dónde buscar los comentarios (ajusta la ruta según tu carpeta)
+  apis: [
+    './src/modules/**/*.js', // Si ejecutas desde la raíz del proyecto
+    './modules/**/*.js'      // Si ejecutas desde dentro de src
+  ],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Importar rutas de user
+const userRoutes = require('./modules/user/user.routes');
+app.use('/api/v1/usuarios', userRoutes);
+
+
+
 // importar y registrar rutas de auth
 const authRoutes = require('./modules/auth/auth.routes');
 app.use('/auth', authRoutes);
