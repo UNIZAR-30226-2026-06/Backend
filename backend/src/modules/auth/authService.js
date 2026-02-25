6756c24712d2b91b077d5e6b276dcb65abbae756
jairojg5 <jairojg2005@gmail.com>
2026-02-25 13:55:32 +0100

    Correccion ficheros servicios accesos BD

--- backend/src/modules/auth/authService.js
+++ backend/src/modules/auth/authService.js
@@ -15,9 +15,9 @@ async function comparePassword(password, hashedPassword) {
 
 async function authenticateUser(nombre_usuario, password) {
     //funcion para autenticar a un usuario comparando la contraseña ingresada con la almacenada en la base de datos
-    const [rows] = await db.query('SELECT contrasena FROM USUARIO WHERE nombre_usuario = ?', [nombre_usuario]);
-    if (rows.length > 0) {
-        const hashedPassword = rows[0].contrasena;
+    const result = await db.query('SELECT contrasena FROM USUARIO WHERE nombre_usuario = $1', [nombre_usuario]);
+    if (result.rows.length > 0) {
+        const hashedPassword = result.rows[0].contrasena;
         return await comparePassword(password, hashedPassword);
     }
     return false;
@@ -25,9 +25,9 @@ async function authenticateUser(nombre_usuario, password) {
 
 //funciones para cambiar contraseña
 async function comprobarContraseñaActualCorrecta(nombre_usuario, contrasena_actual) {
-    const [rows] = await db.query('SELECT contrasena FROM USUARIO WHERE nombre_usuario = ?', [nombre_usuario]);
-    if (rows.length > 0) {
-        const contrasenaAlmacenada = rows[0].contrasena;
+    const result = await db.query('SELECT contrasena FROM USUARIO WHERE nombre_usuario = $1', [nombre_usuario]);
+    if (result.rows.length > 0) {
+        const contrasenaAlmacenada = result.rows[0].contrasena;
         return await comparePassword(contrasena_actual, contrasenaAlmacenada);
     }
     return false;
--- backend/src/modules/friends/friendsService.js
+++ backend/src/modules/friends/friendsService.js
@@ -2,52 +2,52 @@ const db = require('../../database');
 
 async function enviar_Solicitud_Amistad(id_sender, id_receiver) {
     //funcion para enviar una solicitud de amistad de un usuario a otro
-    const [result] = await db.query('INSERT INTO SOLICITUD_AMISTAD (id_usuario_origen, id_usuario_destino, estado) VALUES (?,?,?)', [id_sender, id_receiver, "pendiente"]);
-    return result.insertId;
+    const result = await db.query('INSERT INTO SOLICITUD_AMISTAD (id_usuario_origen, id_usuario_destino, estado) VALUES ($1,$2,$3)', [id_sender, id_receiver, "pendiente"]);
+    return result.rowCount;
 }
 
 async function eliminar_solicitud_amistad(id_sender, id_receiver) {
     //funcion para eliminar una solicitud de amistad de un usuario a otro
-    const [result] = await db.query('DELETE FROM SOLICITUD_AMISTAD WHERE id_usuario_origen=? AND id_usuario_destino=?', [id_sender, id_receiver, "pendiente"]);
+    const result = await db.query('DELETE FROM SOLICITUD_AMISTAD WHERE id_usuario_origen=$1 AND id_usuario_destino=$2 AND estado=$3', [id_sender, id_receiver, "pendiente"]);
     return result;
 }
 
 async function obtener_Solicitudes_Pendientes(id) {
-    const [rows] = await db.query('SELECT * FROM SOLICITUD_AMISTAD WHERE id_usuario_destino=? and estado="pendiente"', [id]);
-    return rows;
+    const result = await db.query('SELECT * FROM SOLICITUD_AMISTAD WHERE id_usuario_destino=$1 and estado=$2', [id, "pendiente"]);
+    return result;
 }
 
 async function aceptar_Solicitud_Amistad(id, id_nuevo_amigo) {
-    const [result] = await db.query('UPDATE SOLICITUD_AMISTAD SET estado="aceptada" WHERE id_usuario_origen=? AND id_usuario_destino=?', [id_nuevo_amigo, id])
-    const [result1] = await db.query('INSERT INTO AMIGOS (id_usuario1, id_usuario2) VALUES (?,?)', [id, id_nuevo_amigo]);
+    const result = await db.query('UPDATE SOLICITUD_AMISTAD SET estado=$1 WHERE id_usuario_origen=$2 AND id_usuario_destino=$3', ["aceptada", id_nuevo_amigo, id])
+    const result1 = await db.query('INSERT INTO AMIGOS (id_usuario1, id_usuario2) VALUES ($1,$2)', [id, id_nuevo_amigo]);
     
     return result1
 }
 
 async function rechazar_Solicitud_Amistad(id, id_nuevo_amigo) {
-    const [result] = await db.query('UPDATE SOLICITUD_AMISTAD SET estado="rechazada" WHERE id_usuario_origen=? AND id_usuario_destino=?', [id_nuevo_amigo, id])
+    const result = await db.query('UPDATE SOLICITUD_AMISTAD SET estado=$1 WHERE id_usuario_origen=$2 AND id_usuario_destino=$3', ["rechazada", id_nuevo_amigo, id])
     return result
 }
 
 async function obtener_count_solicitudes_pendientes(id) {
-    const [result]=await db.query('SELECT COUNT(*) AS total FROM SOLICITUD_AMISTAD WHERE id_usuario_destino=? and estado="pendiente"', [id]);
-    return result[0].total;
+    const result=await db.query('SELECT COUNT(*) AS total FROM SOLICITUD_AMISTAD WHERE id_usuario_destino=$1 and estado=$2', [id, "pendiente"]);
+    return result.rows[0].total;
 }
 
 async function obtener_count_amigos(id) {
-    const [result]=await db.query('SELECT COUNT(*) AS total FROM AMIGOS WHERE (id_usuario1=? OR id_usuario2=?)', [id, id]);
-    return result[0].total;
+    const result=await db.query('SELECT COUNT(*) AS total FROM AMIGOS WHERE (id_usuario1=$1 OR id_usuario2=$2)', [id, id]);
+    return result.rows[0].total;
 }
 
 async function obtener_amigos(id) {
-    const [result]=await db.query('SELECT * FROM AMIGOS WHERE (id_usuario1=? OR id_usuario2=?)', [id, id]);
+    const result=await db.query('SELECT * FROM AMIGOS WHERE (id_usuario1=$1 OR id_usuario2=$2)', [id, id]);
     let lista =[]
     //bucle que recorre la lista para poder devolver solo una array con los ids de los amigos de "id", asi no se devuelve tupla (id1,id2) donde uno de ellos es "id"
-    for (i of result) {
+    for (const i of result.rows) {
         if (i.id_usuario1 != id) {
-            lista.push(id_usuario1)
+            lista.push(i.id_usuario1)
         } else {
-            lista.push(id_usuario2)
+            lista.push(i.id_usuario2)
         }
     }
 
@@ -56,11 +56,13 @@ async function obtener_amigos(id) {
 
 async function eliminar_amigo(id, id_amigo_eliminar) {
     //funcion para elimianr una solicitud de amistad de un usuario a otro
-    const [result] = await db.query('DELETE FROM AMIGOS WHERE ((id_usuario1=? AND id_usuario2=?) OR (id_usuario1=? AND id_usuario2=?))', [id, id_amigo_eliminar, id_amigo_eliminar, id]);
-    return result;
+    const result = await db.query('DELETE FROM AMIGOS WHERE ((id_usuario1=$1 AND id_usuario2=$2) OR (id_usuario1=$3 AND id_usuario2=$4))', [id, id_amigo_eliminar, id_amigo_eliminar, id]);
+    const result1 = await db.query('DELETE FROM SOLICITUD_AMISTAD WHERE ((id_usuario_origen=$1 AND id_usuario_destino=$2) OR (id_usuario_origen=$3 AND id_usuario_destino=$4))', [id, id_amigo_eliminar, id_amigo_eliminar, id]);
+    
+    return result1;
 }
 
 async function buscar_amigos(string_buscar) {
-    const [result]=await db.query('SELECT nombre_usuario FROM USUARIO WHERE (nombre_usuario LIKE ?)', [`%${string_buscar}%`]);
-    
+    const result=await db.query('SELECT nombre_usuario FROM USUARIO WHERE nombre_usuario ILIKE $1', [`%${string_buscar}%`]);
+    return result.rows
 }
