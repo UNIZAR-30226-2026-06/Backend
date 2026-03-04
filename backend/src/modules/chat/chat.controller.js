cb357facb6fe9b168f5ebc0281b0dda743e7077d
a896797 <896797@unizar.es>
2026-03-04 09:27:29 +0100

    Controller y Routes Chat

---
+++ backend/src/modules/chat/chat.controller.js
@@ -0,0 +1,20 @@
+const chatService = require('./chat.service');
+
+exports.sendMatchMessage = (req, res) => {
+    try {
+        const { mensaje } = req.body;
+        const nombre_usuario = req.user.nombre_usuario;
+        const mensajeProcesado = chatService.processMatchMessage (
+            nombre_usuario,
+            mensaje
+        );
+        return res.status(200).json(mensajeProcesado);
+    }catch(error){
+        if(error.message === 'Mensaje vacío'){
+            return res.status(400).json({message: error.message});
+        }
+        return res.status(500).json({message: 'Error procesando mensaje.'});
+    }
+
+    
+};
--- backend/src/modules/chat/chat.routes.js
+++ backend/src/modules/chat/chat.routes.js
@@ -5,21 +5,6 @@ const authMiddleware = require('../../middlewares/auth.middleware');
 
 router.use(authMiddleware);
 
-router.post('/match', (req, res) => {
-    try {
-        const { mensaje } = req.body;
-        const nombre_usuario = req.user.nombre_usuario;
-        const mensajeProcesado = chatService.processMatchMessage (
-            nombre_usuario,
-            mensaje
-        );
-        return res.status(200).json(mensajeProcesado);
-    }catch(error){
-        if(error.message === 'Mensaje vacío'){
-            return res.status(400).json({message: error.message});
-        }
-        return res.status(500).json({message: 'Error procesando mensaje.'});
-    }
-});
-
+router.post('/match', chatController, (req, res));
+   
 module.exports = router;
