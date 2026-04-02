d5deb1bc303b43d0215d3ae25a0a274b2e9dfb91
jairojg5 <jairojg2005@gmail.com>
2026-04-02 15:42:47 +0200

    update

--- backend/docs/js/app.js
+++ backend/docs/js/app.js
@@ -273,6 +273,278 @@
         }
       },
       "x-parser-unique-object-id": "amigoDesconectado"
+    },
+    "start_game": {
+      "address": "game.start_game",
+      "parameters": {},
+      "messages": {
+        "start_game": {
+          "name": "start_game",
+          "title": "Start Game",
+          "summary": "Crear e iniciar partida en el servidor,",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "jugadores": {
+                "type": "integer",
+                "x-parser-schema-id": "<anonymous-schema-20>"
+              },
+              "configuracion": {
+                "type": "object",
+                "properties": {
+                  "maxJugadores": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-22>"
+                  },
+                  "numCartasInicio": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-23>"
+                  },
+                  "timeoutTurno": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-24>"
+                  },
+                  "privada": {
+                    "type": "boolean",
+                    "x-parser-schema-id": "<anonymous-schema-25>"
+                  },
+                  "modoCartasEspeciales": {
+                    "type": "boolean",
+                    "x-parser-schema-id": "<anonymous-schema-26>"
+                  },
+                  "modoRoles": {
+                    "type": "boolean",
+                    "x-parser-schema-id": "<anonymous-schema-27>"
+                  }
+                },
+                "x-parser-schema-id": "<anonymous-schema-21>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-19>"
+          },
+          "x-parser-unique-object-id": "start_game"
+        }
+      },
+      "x-parser-unique-object-id": "start_game"
+    },
+    "partida_iniciada": {
+      "address": "game.partida_iniciada",
+      "parameters": {},
+      "messages": {
+        "partida_iniciada": {
+          "name": "partida_iniciada",
+          "title": "Partida Iniciada",
+          "summary": "se le indica a todos los jugadores de la partida que la partida ha comenzado y se les envia el mazo inicial",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "partidaID": {
+                "type": "string",
+                "x-parser-schema-id": "<anonymous-schema-29>"
+              },
+              "mazoInicial": {
+                "type": "array",
+                "items": {
+                  "type": "object",
+                  "properties": {
+                    "id_carta": {
+                      "type": "integer",
+                      "x-parser-schema-id": "<anonymous-schema-32>"
+                    },
+                    "tipo_carta": {
+                      "type": "string",
+                      "x-parser-schema-id": "<anonymous-schema-33>"
+                    },
+                    "color": {
+                      "type": "string",
+                      "x-parser-schema-id": "<anonymous-schema-34>"
+                    },
+                    "numero": {
+                      "type": "integer",
+                      "x-parser-schema-id": "<anonymous-schema-35>"
+                    },
+                    "codigo": {
+                      "type": "string",
+                      "x-parser-schema-id": "<anonymous-schema-36>"
+                    },
+                    "id_estilo": {
+                      "type": "integer",
+                      "x-parser-schema-id": "<anonymous-schema-37>"
+                    }
+                  },
+                  "x-parser-schema-id": "<anonymous-schema-31>"
+                },
+                "x-parser-schema-id": "<anonymous-schema-30>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-28>"
+          },
+          "x-parser-unique-object-id": "partida_iniciada"
+        }
+      },
+      "x-parser-unique-object-id": "partida_iniciada"
+    },
+    "comprobar_turno": {
+      "address": "game.comprobar_turno",
+      "parameters": {},
+      "messages": {
+        "comprobar_turno": {
+          "name": "comprobar_turno",
+          "title": "Comprobar Turno",
+          "summary": "El jugador que ha jugado el turno emite este evento para comprobar si el turno es valido",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "partidaID": {
+                "type": "string",
+                "x-parser-schema-id": "<anonymous-schema-39>"
+              },
+              "cartaId": {
+                "type": "integer",
+                "x-parser-schema-id": "<anonymous-schema-40>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-38>"
+          },
+          "x-parser-unique-object-id": "comprobar_turno"
+        }
+      },
+      "x-parser-unique-object-id": "comprobar_turno"
+    },
+    "turno_invalido": {
+      "address": "game.turno_invalido",
+      "parameters": {},
+      "messages": {
+        "turno_invalido": {
+          "name": "turno_invalido",
+          "title": "Turno Invalido",
+          "summary": "Se le indica al jugador que ha jugado el turno que el turno no es valido, con un mensaje de error",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "message": {
+                "type": "string",
+                "x-parser-schema-id": "<anonymous-schema-42>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-41>"
+          },
+          "x-parser-unique-object-id": "turno_invalido"
+        }
+      },
+      "x-parser-unique-object-id": "turno_invalido"
+    },
+    "turno_siguiente": {
+      "address": "game.turno_siguiente",
+      "parameters": {},
+      "messages": {
+        "turno_siguiente": {
+          "name": "turno_siguiente",
+          "title": "Turno Siguiente",
+          "summary": "Se le indica a todos los jugadores de la partida quien es el siguiente jugador y si tiene que robar cartas o no",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "siguienteJugador": {
+                "type": "string",
+                "x-parser-schema-id": "<anonymous-schema-44>"
+              },
+              "cartasRobar": {
+                "type": "integer",
+                "x-parser-schema-id": "<anonymous-schema-45>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-43>"
+          },
+          "x-parser-unique-object-id": "turno_siguiente"
+        }
+      },
+      "x-parser-unique-object-id": "turno_siguiente"
+    },
+    "robar_carta": {
+      "address": "game.robar_carta",
+      "parameters": {},
+      "messages": {
+        "robar_carta": {
+          "name": "robar_carta",
+          "title": "Robar Carta",
+          "summary": "El jugador que tiene que robar una carta emite este evento para robar la carta",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "partidaID": {
+                "type": "string",
+                "x-parser-schema-id": "<anonymous-schema-47>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-46>"
+          },
+          "x-parser-unique-object-id": "robar_carta"
+        }
+      },
+      "x-parser-unique-object-id": "robar_carta"
+    },
+    "carta_robada": {
+      "address": "game.carta_robada",
+      "parameters": {},
+      "messages": {
+        "carta_robada": {
+          "name": "carta_robada",
+          "title": "Carta Robada",
+          "summary": "Se le envia al jugador que ha robado la carta la carta robada",
+          "payload": {
+            "type": "object",
+            "properties": {
+              "carta": {
+                "type": "object",
+                "properties": {
+                  "id_carta": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-50>"
+                  },
+                  "tipo_carta": {
+                    "type": "string",
+                    "x-parser-schema-id": "<anonymous-schema-51>"
+                  },
+                  "color": {
+                    "type": "string",
+                    "x-parser-schema-id": "<anonymous-schema-52>"
+                  },
+                  "numero": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-53>"
+                  },
+                  "codigo": {
+                    "type": "string",
+                    "x-parser-schema-id": "<anonymous-schema-54>"
+                  },
+                  "id_estilo": {
+                    "type": "integer",
+                    "x-parser-schema-id": "<anonymous-schema-55>"
+                  }
+                },
+                "x-parser-schema-id": "<anonymous-schema-49>"
+              }
+            },
+            "x-parser-schema-id": "<anonymous-schema-48>"
+          },
+          "x-parser-unique-object-id": "carta_robada"
+        }
+      },
+      "x-parser-unique-object-id": "carta_robada"
+    },
+    "disconnect": {
+      "address": "disconnect",
+      "parameters": {},
+      "messages": {
+        "disconnect": {
+          "name": "disconnect",
+          "title": "Disconnect",
+          "summary": "Un cliente notifica que se ha desconectado al servidor, para eliminarlo de la lista de usuarios conectados",
+          "x-parser-unique-object-id": "disconnect"
+        }
+      },
+      "x-parser-unique-object-id": "disconnect"
     }
   },
   "operations": {
@@ -345,6 +617,46 @@
       "action": "send",
       "channel": "$ref:$.channels.amigoDesconectado",
       "x-parser-unique-object-id": "amigoDesconectado"
+    },
+    "start_game": {
+      "action": "receive",
+      "channel": "$ref:$.channels.start_game",
+      "x-parser-unique-object-id": "start_game"
+    },
+    "partida_iniciada": {
+      "action": "send",
+      "channel": "$ref:$.channels.partida_iniciada",
+      "x-parser-unique-object-id": "partida_iniciada"
+    },
+    "comprobar_turno": {
+      "action": "receive",
+      "channel": "$ref:$.channels.comprobar_turno",
+      "x-parser-unique-object-id": "comprobar_turno"
+    },
+    "turno_invalido": {
+      "action": "send",
+      "channel": "$ref:$.channels.turno_invalido",
+      "x-parser-unique-object-id": "turno_invalido"
+    },
+    "turno_siguiente": {
+      "action": "send",
+      "channel": "$ref:$.channels.turno_siguiente",
+      "x-parser-unique-object-id": "turno_siguiente"
+    },
+    "robar_carta": {
+      "action": "receive",
+      "channel": "$ref:$.channels.robar_carta",
+      "x-parser-unique-object-id": "robar_carta"
+    },
+    "carta_robada": {
+      "action": "send",
+      "channel": "$ref:$.channels.carta_robada",
+      "x-parser-unique-object-id": "carta_robada"
+    },
+    "disconnect": {
+      "action": "receive",
+      "channel": "$ref:$.channels.disconnect",
+      "x-parser-unique-object-id": "disconnect"
     }
   },
   "components": {
@@ -362,7 +674,15 @@
       "avisarAmigosConectados_UserOnline": "$ref:$.channels.avisarAmigosConectados_UserOnline.messages.avisarAmigosConectados_UserOnline",
       "amigoConectado": "$ref:$.channels.amigoConectado.messages.amigoConectado",
       "avisarAmigosConectados_UserDisconnect": "$ref:$.channels.avisarAmigosConectados_UserDisconnect.messages.avisarAmigosConectados_UserDisconnect",
-      "amigoDesconectado": "$ref:$.channels.amigoDesconectado.messages.amigoDesconectado"
+      "amigoDesconectado": "$ref:$.channels.amigoDesconectado.messages.amigoDesconectado",
+      "start_game": "$ref:$.channels.start_game.messages.start_game",
+      "partida_iniciada": "$ref:$.channels.partida_iniciada.messages.partida_iniciada",
+      "comprobar_turno": "$ref:$.channels.comprobar_turno.messages.comprobar_turno",
+      "turno_invalido": "$ref:$.channels.turno_invalido.messages.turno_invalido",
+      "turno_siguiente": "$ref:$.channels.turno_siguiente.messages.turno_siguiente",
+      "robar_carta": "$ref:$.channels.robar_carta.messages.robar_carta",
+      "carta_robada": "$ref:$.channels.carta_robada.messages.carta_robada",
+      "disconnect": "$ref:$.channels.disconnect.messages.disconnect"
     }
   },
   "x-parser-spec-parsed": true,
